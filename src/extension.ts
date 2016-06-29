9e162d4fe31c3eeffc6e8fa69c7586102c8fb3e3
Hamza Hawkins <hhawkins@users.noreply.github.com>
2016-06-29 09:30:13 -0700

    Starting to put together the structure

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+// var funcy = require("funcy-azure");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -14,11 +15,25 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposable = vscode.commands.registerCommand('extension.azureFunctions', () => {
         // The code you place here will be executed every time your command is executed
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        // Give the user some options
+        Promise.resolve(vscode.window.showQuickPick(['Create a new function...', 'Publish this function...', 'Run this function...']))
+            .then(answer => {
+
+                if (answer == 'Crete a new function...') {
+                    vscode.window.showInformationMessage("Let's create a new Azure Function...");
+                }
+
+                if (answer == 'Publish this function...') {
+                    vscode.window.showInformationMessage("Let's publish this function to your Azure account...");
+                }
+
+                if (answer == 'Run this function...') {
+                    vscode.window.showInformationMessage("Let's run this function locally...");
+                }
+            });
     });
 
     context.subscriptions.push(disposable);
