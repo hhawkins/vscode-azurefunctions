7d69b54d66c29b31231756af4e805dd4ddfc3b2a
Hamza Hawkins <hhawkins@users.noreply.github.com>
2016-07-19 17:05:13 -0700

    Added prompts to separate the templates

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,11 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as WebRequest from 'web-request';
+// import * as requestPromise from 'request-promise';
+
+// Import language.json for downloading the correct files in the template
+// var languagesJSON = require('./languages.json');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -20,14 +25,13 @@ export function activate(context: vscode.ExtensionContext) {
         // Give the user some options
         Promise.resolve(vscode.window.showQuickPick(['Create a new function...', 'Publish this function...', 'Run this function...']))
             .then(answer => {
-                vscode.window.showInformationMessage("You chose " + answer);
-
                 if (answer == 'Create a new function...') {
                     if (vscode.workspace.rootPath == undefined) {
                         // Make sure a workspace is setup
                         vscode.window.showErrorMessage("Open a folder first...");
                     } else {
                         // Start the process to create a function
+                        console.log(vscode.workspace.rootPath);
                         createAzureFunction();
                     }
                 }
@@ -44,7 +48,68 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 function createAzureFunction () {
-    
+    // URL for the templates.json file
+    var templatesUrl = "https://ahmelsayed.blob.core.windows.net/public/templates.json";
+    var templatesJson = {};
+
+    (async function () {
+        templatesJson = await WebRequest.json<any>(templatesUrl);
+        console.log(templatesJson);
+
+        var sortedTemplatesByLanguage = {};
+
+        for (var i in templatesJson) {
+            var tempID = templatesJson[i].id;
+            var tempLang = templatesJson[i].metadata.language;
+
+            if (sortedTemplatesByLanguage.hasOwnProperty(tempLang)) {
+                sortedTemplatesByLanguage[tempLang].push(tempID);
+            } else {
+                sortedTemplatesByLanguage[tempLang] = new Array(tempID);
+            }
+        }
+        
+        console.log('asking user for input...');
+
+        // Ask the user to pick a language to use
+        var chosenLanguage = "";
+        await Promise.resolve(vscode.window.showQuickPick(Object.keys(sortedTemplatesByLanguage)))
+        .then(language => {
+            chosenLanguage = language;
+        });
+
+        console.log('chosenLanguage: ' + chosenLanguage);
+
+        // Ask the user to select the template based on the language
+        var chosenTemplate = "";
+        await Promise.resolve(vscode.window.showQuickPick(sortedTemplatesByLanguage[chosenLanguage]))
+        .then(answer => {
+            chosenTemplate = answer;
+        });
+
+        console.log('chosenTemplate: ' + chosenTemplate);
+
+        // Download the template
+        var githubApiUrl = "https://api.github.com/repos/Azure/azure-webjobs-sdk-templates/contents/Templates";
+        var branchIdentifier = "?ref=dev";
+
+        // JSON full of files of the template to download
+        // var templateToDownloadJson = await WebRequest.json<any>(githubApiUrl + chosenTemplate + branchIdentifier);
+        var urlForTemplateFiles = githubApiUrl + chosenTemplate + branchIdentifier;
+
+        var options = {
+            uri: urlForTemplateFiles,
+        headers: {
+            'User-Agent': 'Request-Promise'
+        },
+            json: true
+        };
+        
+        // console.log(templateToDownloadJson);
+        // for (var file in templateToDownloadJson) {
+            // console.log('file: ' + file);
+        // }
+    })();
 }
 
 // this method is called when your extension is deactivated
