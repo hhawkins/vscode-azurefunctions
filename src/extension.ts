51f2b919b0ada1e312ec58dba739d1eea687dc37
Hamza Hawkins <hhawkins@users.noreply.github.com>
2016-08-15 18:23:58 -0700

    Completely rewrote everything with promises and added comments making it much better to edit

--- src/extension.ts
+++ src/extension.ts
@@ -22,11 +22,15 @@ var foldersToExclude = [
 
 var optionsForUser = [
     "Create a new function...",
-    // "Publish function...",
-    "Run a function..."
+    "Run a function...",
+    "Log into azure portal...",
+    "List function apps in current tenant..."
 ];
 
-// this method is called when your extension is activated
+// Setup the output channel to display the results of the CLI
+var outputChannel = vscode.window.createOutputChannel("Azure Functions");
+
+// This method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
@@ -35,62 +39,76 @@ export function activate(context: vscode.ExtensionContext) {
     console.log('Congratulations, your extension "vscode-azurefunctions" is now active!');
 
     // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
+    // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.azureFunctions', () => {
         // The code you place here will be executed every time your command is executed
 
-        // Give the user some options
-        var userChoice = vscode.window.showQuickPick(optionsForUser);
-        userChoice
-            .then(answer => {
-                if (answer == 'Create a new function...') {
-                    if (vscode.workspace.rootPath == undefined) {
-                        // Make sure a workspace is setup
-                        vscode.window.showErrorMessage("Open a folder first!");
-                    } else {
-                        // Start the process to create a function
-                        console.log(vscode.workspace.rootPath);
-                        createAzureFunction();
-                    }
-                } else if (answer == 'Publish function...') {
-                    vscode.window.showInformationMessage("Feature coming soon!");
+        // Lets give the user some options
+        // **Note: The create and run functionality has unique implementations when compared to other CLI commands
+        // ** Refer to them for implementing more complex interactions below 
+        // var userChoice = vscode.window.showQuickPick(optionsForUser);
+        // Promise.prototype
+        Promise.resolve(vscode.window.showQuickPick(optionsForUser))
+        .then(answer => {
+            // Run the 'login' command in the CLI
+            if (answer == 'Log into azure portal...') {
+                runCLI('login');
+            } 
+            
+            // Run the 'list' command in the CLI
+            else if (answer == 'List function apps in current tenant...') {
+                runCLI('list');
+            }
 
-                } else if (answer == 'Run a function...') {
-                    // For demo purposes
-                    // vscode.window.showInformationMessage("Feature coming soon!");
+            // Have the user go through a process to create a new function
+            // Because of the nature of input and output, the process to create a new function is handled through the
+            // createAzureFunction() function below and not through the cli itself
+            else if (answer == 'Create a new function...') {
+                if (vscode.workspace.rootPath == undefined) {
+                    // Make sure a workspace is setup
+                    vscode.window.showErrorMessage("Open a folder first!");
+                } else {
+                    // Start the process to create a function
+                    console.log(vscode.workspace.rootPath);
+                    createAzureFunction();
+                }
+            }
 
-                    if (vscode.workspace.rootPath == undefined) {
-                        // Make sure a workspace is setup
-                        vscode.window.showErrorMessage("Open a folder first!");
-                    } else {
-                        // Start the process to create a function
-                        var listOfFunctions = getDirectories(vscode.workspace.rootPath);
+            // Run a user specified function
+            else if (answer == 'Run a function...') {
+                if (vscode.workspace.rootPath == undefined) {
+                    // Make sure a workspace is setup
+                    vscode.window.showErrorMessage("Open a folder first!");
+                } else {
+                    // Start the process to run a function
+                    var listOfFunctions = getDirectories(vscode.workspace.rootPath);
 
-                        Promise.resolve(vscode.window.showQuickPick(listOfFunctions))
-                            .then(functionName => {
-                                var prompt = "Enter arguments";
-                                vscode.window.showInputBox({
-                                    placeHolder: "Arguments",
-                                    prompt: prompt,
-                                    value: ''
-                                })
-                                .then(userArgument => {
-                                     if (userArgument.length > 0) {
-                                        runCLI('run', functionName, userArgument);
-                                    } else {
-                                        runCLI('run', functionName);
-                                    }
-                                });
-                            }, reason => {
-                                console.log(reason);
+                    // Ask the user to enter arguments
+                    var userArgument = vscode.window.showQuickPick(listOfFunctions);
+                    if (userArgument != undefined) {
+                        userArgument.then(functionName => {
+                            var prompt = "Enter arguments";
+                            vscode.window.showInputBox({
+                                placeHolder: "Arguments",
+                                prompt: prompt,
+                                value: ''
                             })
-                            .catch(error => {
-                                console.log("error: " + error);
+                            .then(userArgument => {
+                                // Run the function that the user chose with arguments
+                                if (userArgument.length > 0) {
+                                    runCLI('run', functionName, userArgument);
+                                } else {
+                                    runCLI('run', functionName);
+                                }
                             });
+                        }, reason => {
+                            console.log(reason);
+                        })
                     }
                 }
-            });
+            }
+        });
     });
 
     context.subscriptions.push(disposable);
@@ -101,69 +119,76 @@ function createAzureFunction () {
     var templatesUrl = "https://ahmelsayed.blob.core.windows.net/public/templates.json";
     var templatesJson = {};
 
-    (async function () {
-        console.log("Creating function...")
-        templatesJson = await WebRequest.json<any>(templatesUrl);
-        console.log(templatesJson);
+    var options = {
+        uri: templatesUrl,
+    headers: {
+        'User-Agent': 'Request-Promise'
+    },
+        json: true
+    };
 
-        var sortedTemplatesByLanguage = {};
+    // Take a look at the templates json file
+    requestPromise(options)
+    .then( templates => {
+        console.log(templates);
 
-        for (var i in templatesJson) {
-            var tempID = templatesJson[i].id;
-            var tempLang = templatesJson[i].metadata.language;
+        // Start going through the process of sorting through the templates json file
+        Promise.resolve(startProcessing(templates));
 
-            if (sortedTemplatesByLanguage.hasOwnProperty(tempLang)) {
-                sortedTemplatesByLanguage[tempLang].push(tempID);
-            } else {
-                sortedTemplatesByLanguage[tempLang] = new Array(tempID);
-            }
+        // The start...
+        function startProcessing (templatesToSort) {
+            return Promise.resolve(sortTheTemplates(templatesToSort));
         }
         
-        console.log('asking user for input...');
+        // Sort the templates by language
+        function sortTheTemplates (templatesToSort) {
+            var sortedTemplatesByLanguage = {};
+
+            for (var i in templatesToSort) {
+                var tempID = templatesToSort[i].id;
+                var tempLang = templatesToSort[i].metadata.language;
+
+                if (sortedTemplatesByLanguage.hasOwnProperty(tempLang)) {
+                    sortedTemplatesByLanguage[tempLang].push(tempID);
+                } else {
+                    sortedTemplatesByLanguage[tempLang] = new Array(tempID);
+                }
+            }
+
+            return Promise.resolve(askUserToPickLanguage(sortedTemplatesByLanguage));
+        }
 
         // Ask the user to pick a language to use
-        var chosenLanguage = "";
-        var languageChoice = vscode.window.showQuickPick(Object.keys(sortedTemplatesByLanguage));
-        languageChoice
-        .then(language => {
-            chosenLanguage = language;
-        }, reason => {
-            console.log(reason);
-        });
-
-        console.log('chosenLanguage: ' + chosenLanguage);
+        function askUserToPickLanguage (sortedTemplates) {
+            console.log('Asking user for language to use...');
+            return Promise.resolve(vscode.window.showQuickPick(Object.keys(sortedTemplates)))
+            .then(language => {
+                if (language != undefined) {
+                    console.log('Language: ' + language);
+                    askUserForTemplate(sortedTemplates, language)
+                } else {
+                    console.log('User did not select a language...');
+                }
+            });
+        }
 
         // Ask the user to select the template based on the language
-        var chosenTemplate = "";
-        var templateChoice = vscode.window.showQuickPick(sortedTemplatesByLanguage[chosenLanguage]);
-        templateChoice
-        .then(answer => {
-            chosenTemplate = answer;
-        }, reason => {
-            console.log(reason);
-        });
-
-        console.log('chosenTemplate: ' + chosenTemplate);
-
-        // Download the template
-        var githubApiUrl = "https://api.github.com/repos/Azure/azure-webjobs-sdk-templates/contents/Templates/";
-        var branchIdentifier = "?ref=dev";
-
-        // JSON full of files of the template to download
-        var urlForTemplateFiles = githubApiUrl + chosenTemplate + branchIdentifier;
-
-        var options = {
-            uri: urlForTemplateFiles,
-        headers: {
-            'User-Agent': 'Request-Promise'
-        },
-            json: true
-        };
+        function askUserForTemplate (sortedTemplates, chosenLanguage) {
+            console.log('Asking user for template to use...');
+            return Promise.resolve(vscode.window.showQuickPick(sortedTemplates[chosenLanguage]))
+            .then(chosenTemplate => {
+                if (chosenTemplate != undefined) {
+                    console.log('Chosen template: ' + chosenTemplate);
+                    askForNameOfFunction(false, "", chosenTemplate);
+                } else {
+                    console.log('User did not choose a template...');
+                }
+            });
+        }
 
         // Ask the user the enter a name for the function
-        var askForNameOfFunction = async function (folderExists = false, folder = "") {
-            var nameForFunction = "";
-            var pathToReturn = "";
+        function askForNameOfFunction(folderExists = false, folder = "", nameOfTemplate = "") {
+            var pathToUse = "";
             var defaultValue = "MyAzureFunction"
             var prompt = "Enter a name for the function (the folder created will have this name)";
 
@@ -172,70 +197,86 @@ function createAzureFunction () {
                 defaultValue = folder;
             }
 
-            await Promise.resolve(vscode.window.showInputBox({
+            Promise.resolve(vscode.window.showInputBox({
                 placeHolder: "Name for function",
                 prompt: prompt,
                 value: defaultValue
             }))
             .then(answer => {
-                nameForFunction = answer;
-            }, reason => {
-                console.log(reason);
-            })
-            .catch(error => {
-                console.log("error: " + error);
+                if (answer != undefined) {
+                    try {
+                        pathToUse = path.resolve(vscode.workspace.rootPath, answer);
+                        fs.mkdirSync(pathToUse);
+                        downloadTheTemplate(nameOfTemplate, answer, pathToUse);
+                    } catch (err) {
+                        console.log("Folder exists");
+                        return askForNameOfFunction(true, answer, nameOfTemplate);
+                    }
+                } else {
+                    console.log('User did not enter a function name...');
+                }
             });
-
-            try {
-                pathToReturn = path.resolve(vscode.workspace.rootPath, nameForFunction);
-                fs.mkdirSync(pathToReturn);
-                return pathToReturn;
-            } catch (err) {
-                console.log("Folder exists");
-                return await askForNameOfFunction(true, nameForFunction);
-            }
+            // var pathToSaveFunction = askForNameOfFunction();
         }
 
-        var pathToSaveFunction = await askForNameOfFunction();
+        function downloadTheTemplate (userChosenTemplate, nameForTheFunction, pathToUse) {
+            // Download the template
+            var githubApiUrl = "https://api.github.com/repos/Azure/azure-webjobs-sdk-templates/contents/Templates/";
+            var branchIdentifier = "?ref=dev";
 
-        // Function to download the files
-        var downloadFiles = function (filesToDownload) {
-            for (let file in filesToDownload) {
-            console.log("downloading " + file);
-            console.log("from " + filesToDownload[file] + "...")
-            console.log(path.resolve(pathToSaveFunction, file))
+            // JSON full of files of the template to download
+            var urlForTemplateFiles = githubApiUrl + userChosenTemplate + branchIdentifier;
 
-            request
-                .get(filesToDownload[file])
-                .on('error', err => {
-                console.log('There was an error when downloading the file ' + file);
-                console.log(err);
-                })
-                .pipe(fs.createWriteStream(path.resolve(pathToSaveFunction, file)));
-            }
-          };
+            var options = {
+                uri: urlForTemplateFiles,
+                headers: {
+                    'User-Agent': 'Request-Promise'
+                },
+                json: true
+            };
 
-        requestPromise(options)
-        .then(templates => {
-            var filesToDownload = {};
+            requestPromise(options)
+            .then(templates => {
+                var filesToDownload = {};
 
-            for (var file in templates) {
-                if (filesToExclude.indexOf(templates[file].name) < 0) {
-                    // Download the file
-                    filesToDownload[templates[file].name] = templates[file].download_url;
+                for (var file in templates) {
+                    if (filesToExclude.indexOf(templates[file].name) < 0) {
+                        // Download the file
+                        filesToDownload[templates[file].name] = templates[file].download_url;
+                    }
                 }
-            }
 
-            console.log("filesToDownload: " + Object.keys(filesToDownload));
-            downloadFiles(filesToDownload);
+                console.log("filesToDownload: " + Object.keys(filesToDownload));
+                downloadFiles(filesToDownload);
 
-            return 1;
-        })
-        .catch(err => {
-            console.log("There was an error in searching through the template");
-            console.log(err);
-        })
-    })();
+                return 1;
+            })
+            .catch(err => {
+                console.log("There was an error in searching through the template");
+                console.log(err);
+            })
+
+            // Function to download the files
+            var downloadFiles = function (filesToDownload) {
+                for (let file in filesToDownload) {
+                console.log("downloading " + file);
+                console.log("from " + filesToDownload[file] + "...")
+                console.log(path.resolve(pathToUse, file))
+
+                request
+                    .get(filesToDownload[file])
+                    .on('error', err => {
+                    console.log('There was an error when downloading the file ' + file);
+                    console.log(err);
+                    })
+                    .pipe(fs.createWriteStream(path.resolve(pathToUse, file)));
+                }
+            };
+        }
+    })
+    .catch( err => {
+        console.log("There was an error in searching for templates");
+    });
 }
 
 function runCLI (command, functionName = null, argument = null) {
@@ -261,8 +302,7 @@ function runCLI (command, functionName = null, argument = null) {
 }
 
 function displayToOutput (textToDisplay) {
-    var outputChannel = vscode.window.createOutputChannel("Azure Functions");
-
+    outputChannel.clear();
     outputChannel.append(textToDisplay);
     outputChannel.show(true);
 }
